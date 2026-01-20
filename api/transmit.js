a8eb616c30f288fcb1c2060badc324d990b16bf3
highland88999-commits <highland88999@gmail.com>
2026-01-20 07:30:45 -0700

    Update transmit.js

--- api/transmit.js
+++ api/transmit.js
@@ -1,3 +1,16 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  // ... rest of your code
+}
+
+
 /**
  * ARCHITECT ARTEMIS | VERCEL BRIDGE
  * Purpose: Routing frontend commands to the Consensus Engine.
