c7ef2646ce2d88208fe9d1b8573bb7e6e85f586c
Merlin Ambros <highland88999@gmail.com>
2026-09-04 15:43:54 -0600

    Update main.py

--- engine/python/main.py
+++ engine/python/main.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-# Bootstrap local engine directory to system path
+# Bootstrap local engine directory to system path[span_6](start_span)[span_6](end_span)
 CURRENT_DIR = Path(__file__).resolve().parent
 if str(CURRENT_DIR) not in sys.path:
     sys.path.insert(0, str(CURRENT_DIR))
